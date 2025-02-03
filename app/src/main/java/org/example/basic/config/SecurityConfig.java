9897e660c2fcf6966fed626ef68d961ff928dabd
vincent-dsmn <vincent.desimone@hotmail.fr>
2025-02-03 13:33:57 +0100

    Configuration security

---
+++ app/src/main/java/org/example/basic/config/SecurityConfig.java
@@ -0,0 +1,30 @@
+package org.example.basic.config;
+
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+    @Bean
+        public SecurityFilterChain filterChain(HttpSecurity http)
+                throws Exception {
+
+            http.authorizeHttpRequests((authz) -> authz
+                            .requestMatchers("/public/**").permitAll()
+                            .requestMatchers("/private/**").authenticated()
+                    )
+                    .sessionManagement(sessionManagement -> 
+                        sessionManagement.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                    ); // On rend les session stateless
+            return http.build();
+    }
+
+
+}
