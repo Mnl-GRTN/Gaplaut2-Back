95dbc7445cfcf3c2843d5584b1f471986e23cc25
vandromm4u <maxime.vandromme1@etu.univ-lorraine.fr>
2025-02-03 11:19:06 +0100

    Test integration creation docteur

---
+++ app/src/test/java/org/example/DoctorServiceIT.java
@@ -0,0 +1,34 @@
+package org.example;
+
+import org.assertj.core.api.Assertions;
+import org.example.repository.CentreRepository;
+import org.example.repository.DoctorRepository;
+import org.example.service.Centre;
+import org.example.service.CentreService;
+import org.example.service.Doctor;
+import org.example.service.DoctorService;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+
+public class DoctorServiceIT {
+    @Autowired
+    DoctorService doctorService;
+
+    @Autowired
+    DoctorRepository doctorRepository;
+
+    @Test
+    public void ITDoctorCreation() {
+        var centre = Mockito.mock(Centre.class);        
+        var doctor = new Doctor(1, "Dupont", "Jean", centre);
+
+        this.doctorRepository.save(doctor);
+
+        var result = doctorService.readOne(1);
+
+        Assertions.assertThat(result.isEqualTo(doctor));
+        
+    
+    }
+}
