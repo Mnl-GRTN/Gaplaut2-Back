bd987b782fd444f6e6dd4aae015b34ae1b2dc39a
vincent-dsmn <vincent.desimone@hotmail.fr>
2025-02-10 20:22:07 +0100

    Update CentreRestController to change API endpoint paths to /public/api

--- app/src/main/java/org/example/rest/CentreRestController.java
+++ app/src/main/java/org/example/rest/CentreRestController.java
@@ -17,17 +17,17 @@ public class CentreRestController {
     @Autowired
     private CentreService service;
 
-    @PostMapping(path = "/api/centres")
+    @PostMapping(path = "/public/api/centres")
     public void create(@RequestBody Centre c) throws URISyntaxException{
         service.create(c);
     }
 
-    @GetMapping(path = "/api/centres")
+    @GetMapping(path = "/public/api/centres")
     public Iterable<Centre> read(){
         return service.readAll();
     }
 
-    @GetMapping(path = "/api/centre/{id}")
+    @GetMapping(path = "/public/api/centre/{id}")
     public Centre readOne(@PathVariable("id") int id){
         return service.readOne(id);
     }
