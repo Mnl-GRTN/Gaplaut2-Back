2c1d2980f8883ac3803a5eef39f58382f7dbc9aa
GARATTONI Manoël <garattoni.manoel@yahoo.com>
2025-02-20 11:37:22 +0100

    Implement password encoding in DoctorService and configure Basic Auth in SecurityConfig

--- app/src/main/java/org/example/basic/config/SecurityConfig.java
+++ app/src/main/java/org/example/basic/config/SecurityConfig.java
@@ -1,21 +1,23 @@
 package org.example.basic.config;
 
-
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
-
+import static org.springframework.security.config.Customizer.withDefaults;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
+
     @Bean
         public SecurityFilterChain filterChain(HttpSecurity http)
                 throws Exception {
-
+            //Basic Auth
             http
                 .csrf((csrf) -> csrf.disable())
                 .authorizeHttpRequests((authz) -> authz
@@ -24,7 +26,15 @@ public class SecurityConfig {
                     )
                     .sessionManagement(sessionManagement -> 
                         sessionManagement.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
-                    ); // On rend les session stateless
+                    ) // Basic Auth is stateless by default
+                    .httpBasic(withDefaults()); 
+
             return http.build();
     }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
 }
