ebcf4a1ac624d68f50c981502a0f94292667b5fb
GARATTONI Manoël <garattoni.manoel@yahoo.com>
2025-02-22 10:01:23 +0100

    Add CustomUserDetailsService for user authentication and configure AuthenticationManager in SecurityConfig

--- app/src/main/java/org/example/basic/config/SecurityConfig.java
+++ app/src/main/java/org/example/basic/config/SecurityConfig.java
@@ -2,6 +2,8 @@ package org.example.basic.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -37,4 +39,9 @@ public class SecurityConfig {
         return new BCryptPasswordEncoder();
     }
 
+    // Auto-configured with CustomUserDetailsService by default
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration authConfig) throws Exception {
+        return authConfig.getAuthenticationManager();
+    }
 }
