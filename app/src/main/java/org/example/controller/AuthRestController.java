0b12f222b67a0ee04aa673bfab7c38250d33c9d3
vincent-dsmn <vincent.desimone@hotmail.fr>
2025-02-25 17:25:01 +0100

    Add doctor centre information to user info endpoint response

--- app/src/main/java/org/example/controller/AuthRestController.java
+++ app/src/main/java/org/example/controller/AuthRestController.java
@@ -2,8 +2,13 @@ package org.example.controller;
 
 import java.util.HashMap;
 import java.util.Map;
+
+import org.example.repository.DoctorRepository;
+import org.example.service.Doctor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.core.GrantedAuthority;
 import java.util.Collection;
 import java.util.stream.Collectors;
@@ -13,6 +18,9 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class AuthRestController {
+
+    @Autowired
+    private DoctorRepository doctorRepository;
 
     @GetMapping("private/api/auth/user-info")
     public Map<String, Object> userInfo() {
@@ -24,6 +32,14 @@ public class AuthRestController {
 
         userInfo.put("username", username);
         userInfo.put("authorities", authorities.stream().map(GrantedAuthority::getAuthority).collect(Collectors.toList()));
+
+        // Get the centre of the doctor
+        Doctor doctor = doctorRepository.findByEmail(username)
+                            .orElseThrow(() -> new UsernameNotFoundException("Utilisateur non trouvé"));
+        Integer doctor_centre = doctor.getCentre().getId();
+        userInfo.put("centre", doctor_centre);
+
+
         return userInfo;
     }
     
