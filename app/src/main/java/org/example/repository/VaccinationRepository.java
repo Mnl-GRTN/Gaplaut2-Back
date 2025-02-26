f141f995ffa87aca68d799e049ecdbafbdb2ab5f
vincent-dsmn <vincent.desimone@hotmail.fr>
2025-02-26 09:49:39 +0100

    Remove unused method from VaccinationRepository

--- app/src/main/java/org/example/repository/VaccinationRepository.java
+++ app/src/main/java/org/example/repository/VaccinationRepository.java
@@ -16,6 +16,4 @@ public interface VaccinationRepository extends JpaRepository<Vaccination, Intege
     //Optional is a container object which may or may not contain a non-null value.
     Iterable<Vaccination> findByCentre_IdAndDate(Integer centreId, LocalDate date);
 
-    Iterable<Vaccination> findByCentre_IdAndDateAndLastName(Integer centreId, LocalDate date, String lastName);
-
 }
