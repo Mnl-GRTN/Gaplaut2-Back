bb36408a7b324f442391643aa1f7867cd08c2962
vincent-dsmn <vincent.desimone@hotmail.fr>
2025-02-28 14:24:28 +0100

    Filter out centre with ID 1 from the list of all centres in CentreService

--- app/src/main/java/org/example/service/CentreService.java
+++ app/src/main/java/org/example/service/CentreService.java
@@ -1,5 +1,9 @@
 package org.example.service;
 
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.StreamSupport;
+
 import org.example.repository.CentreRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -15,7 +19,17 @@ public class CentreService {
     }
 
     public Iterable<Centre> readAll(){
-        return centreRepository.findAll();
+        List<Centre> centres = centreRepository.findAll();
+
+        centres = StreamSupport.stream(centres.spliterator(), false)
+        .filter(obj -> obj.getId() != 1)
+        .collect(Collectors.toList());
+
+        
+        
+        
+        
+        return centres;
     }
 
     public Centre readOne(int id){
