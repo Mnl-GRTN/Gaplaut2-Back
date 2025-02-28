43d36005f6e47384698d11fcaf685d811fd63b01
vincent-dsmn <vincent.desimone@hotmail.fr>
2025-02-28 11:49:49 +0100

    Create an Admin center for superadmin

--- app/src/main/java/org/example/basic/config/DataBaseInit.java
+++ app/src/main/java/org/example/basic/config/DataBaseInit.java
@@ -37,11 +37,11 @@ public class DataBaseInit {
 
     @PostConstruct
     public void init() {
-        Centre centre1 = new Centre(1, "CHU NANCY", "NANCY", "1 rue de la santé", "54000");
-        Centre centre2 = new Centre(2, "CHU STRASBOURG", "STRASBOURG", "2 rue de la santé", "67000");
+        Centre centre1 = new Centre(1, "Centre Administration", "PARIS", "4 rue de la santé", "75000");
+        Centre centre2 = new Centre(2, "CHU NANCY", "NANCY", "1 rue de la santé", "54000");
+        Centre centre3 = new Centre(3, "CHU STRASBOURG", "STRASBOURG", "2 rue de la santé", "67000");
 
         if (centreRepository.count() == 0) {
-            Centre centre3 = new Centre(3, "CHU METZ", "METZ", "3 rue de la santé", "57000");
             Centre centre4 = new Centre(4, "CHU PARIS", "PARIS", "4 rue de la santé", "75000");
             Centre centre5 = new Centre(5, "CHU LYON", "LYON", "5 rue de la santé", "69000");
             Centre centre6 = new Centre(6, "CHU MARSEILLE", "MARSEILLE", "6 rue de la santé", "13000");
@@ -73,28 +73,28 @@ public class DataBaseInit {
         if (doctorRepository.count() == 0) {
 
             // Create superadmin
-            Doctor superadmin = new Doctor(1, "superadmin", "superadmin", null, "superadmin", "superadmin",
+            Doctor superadmin = new Doctor(1, "superadmin", "superadmin",centre1, "superadmin", "superadmin",
                 Collections.singleton(role1));
             superadmin.setPassword(passwordEncoder.encode(superadmin.getPassword()));
             doctorRepository.save(superadmin);
 
             // Create admin for the first two centres
-            Doctor admin = new Doctor(2, "admin", "admin", centre1, "admin", "admin", Collections.singleton(role2));
+            Doctor admin = new Doctor(2, "admin", "admin", centre2, "admin", "admin", Collections.singleton(role2));
             admin.setPassword(passwordEncoder.encode(admin.getPassword()));
             doctorRepository.save(admin);
 
-            Doctor admin2 = new Doctor(3, "admin2", "admin2", centre2, "admin2", "admin2",
+            Doctor admin2 = new Doctor(3, "admin2", "admin2", centre3, "admin2", "admin2",
                 Collections.singleton(role2));
             admin2.setPassword(passwordEncoder.encode(admin2.getPassword()));
             doctorRepository.save(admin2);
 
             // Create doctors for the first two centres
-            Doctor doctor = new Doctor(4, "doctor", "doctor", centre1, "doctor", "doctor",
+            Doctor doctor = new Doctor(4, "doctor", "doctor", centre2, "doctor", "doctor",
                 Collections.singleton(role3));
             doctor.setPassword(passwordEncoder.encode(doctor.getPassword()));
             doctorRepository.save(doctor);
 
-            Doctor doctor2 = new Doctor(5, "doctor2", "doctor2", centre2, "doctor2", "doctor2",
+            Doctor doctor2 = new Doctor(5, "doctor2", "doctor2", centre3, "doctor2", "doctor2",
                 Collections.singleton(role3));
             doctor2.setPassword(passwordEncoder.encode(doctor2.getPassword()));
             doctorRepository.save(doctor2);
@@ -103,10 +103,10 @@ public class DataBaseInit {
         if(vaccinationRepository.count() == 0) {
             LocalDate date = LocalDate.of(2021, 12, 12);
             LocalDate date2 = LocalDate.of(2021, 12, 13);
-            Vaccination vaccination = new Vaccination(centre1, "john.doe@gmail.com", "0606060606", "Doe", "John", date);
-            Vaccination vaccination2 = new Vaccination(centre1, "jane.doe@gmail.com", "0707070707", "Doe", "Jane", date);
-            Vaccination vaccination3 = new Vaccination(centre2, "jean.dupont@gmail.com", "0808080808", "Dupont", "Jean", date);
-            Vaccination vaccination4 = new Vaccination(centre2, "pierre.paul@gmail.com", "0909090909", "Paul", "Pierre", date2);
+            Vaccination vaccination = new Vaccination(centre2, "john.doe@gmail.com", "0606060606", "Doe", "John", date);
+            Vaccination vaccination2 = new Vaccination(centre2, "jane.doe@gmail.com", "0707070707", "Doe", "Jane", date);
+            Vaccination vaccination3 = new Vaccination(centre3, "jean.dupont@gmail.com", "0808080808", "Dupont", "Jean", date);
+            Vaccination vaccination4 = new Vaccination(centre3, "pierre.paul@gmail.com", "0909090909", "Paul", "Pierre", date2);
 
             vaccinationRepository.save(vaccination);
             vaccinationRepository.save(vaccination2);
